6bc7e5891836783255d3e736de005cd671df1172
L <3049152784@qq.com>
2025-09-17 22:52:10 +0800

    错误类文件

---
+++ src/exceptions.js
@@ -0,0 +1,53 @@
+/**
+ * 自定义错误基类
+ */
+class BaseError extends Error {
+    // 错误码
+    errorCode
+    // 自定义错误信息
+    message
+    // 响应码
+    status
+
+    constructor(message = '网络异常', errorCode = '1000', status = 200) {
+        super(message)
+        this.message = message
+        this.errorCode = errorCode
+        this.status = status        
+    }
+
+    toJSON() {
+        return JSON.stringify({
+            message: this.message,
+            errorCode: this.errorCode
+        })
+    }
+}
+
+
+/**
+ * 请求参数错误
+ */
+class ParamError extends BaseError {
+    constructor(message = '参数有误', errorCode = '10000', status = 400) {
+        super(message, errorCode, status)
+    }
+
+}
+
+
+
+/**
+ * 服务器异常
+ */
+class ServerError extends BaseError {
+        constructor(message = '服务器异常', errorCode = '999', status = 500) {
+        super(message, errorCode, status)
+    }
+
+}
+
+
+module.exports = {
+    BaseError, ServerError, ParamError
+}
