9bc2fd80bc5609804db164b5f230d0cf525dfec4
L <3049152784@qq.com>
2025-10-10 01:31:35 +0800

    修改路由实现方式, 增加一级路由. 注册七牛云路由

--- src/rotuer/index.js
+++ src/rotuer/index.js
@@ -1,8 +1,16 @@
+const Router = require('koa-router')
 const user_router = require('./user.js')
+const qiniu_router = require('./qiniu.js')
 
 
+const router = new Router({
+    prefix: '/api'  // 路由前缀
+})
+
+router.use(user_router.routes()) // 挂载用户路由
+router.use(qiniu_router.routes()) // 挂载七牛云路由
+
+// 导出路由
 module.exports = app => {
-    /* 用户路由 */
-    app.use(user_router.routes()).use(user_router.allowedMethods())
-    
+    app.use(router.routes()).use(router.allowedMethods())
 }
