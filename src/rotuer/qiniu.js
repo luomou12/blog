3bd28fa0274da32f5cb88a68ececc350644022f5
L <3049152784@qq.com>
2025-10-13 19:54:29 +0800

    新增七牛云路由-实现mkdir创建文件夹方法

--- src/rotuer/qiniu.js
+++ src/rotuer/qiniu.js
@@ -1,9 +1,29 @@
 const Router = require('koa-router');
+const File = require('../api/file.js');
+const { ParamError } = require('../exceptions.js');
+
 
 
 const qiniu_router = new Router({
     prefix: '/qiniu/'
 })
+
+
+qiniu_router.post('mkdir', (ctx) => {
+    const user = ctx.user
+    const body = ctx.request.body
+    body.tag = (body.tag || '/').trim()
+    const dir_name = body.dir_name ? body.dir_name.trim() : ''
+
+    // 参数校验
+    if (!dir_name) throw new ParamError('文件夹名称不能为空')
+    
+    return File.mkdir(user, dir_name, body.tag)
+    
+})
+
+
+
 
 
 qiniu_router.post('upload', (ctx) => {
