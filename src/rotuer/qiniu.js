2b23bfa757862b1cff6ed287c4d54142b0933704
L <3049152784@qq.com>
2025-10-15 19:32:48 +0800

    新增处理文件上传中间件方法

--- src/rotuer/qiniu.js
+++ src/rotuer/qiniu.js
@@ -1,6 +1,26 @@
 const Router = require('koa-router');
 const File = require('../api/file.js');
-const { ParamError } = require('../exceptions.js');
+const { ParamError, FileUploadError } = require('../exceptions.js');
+
+
+// 处理文件上传
+const multer = require('multer')
+const upload = multer().single('file')
+// 文件上传中间件
+const upload_midd_ware = async (ctx, next) => {
+    const { req, res } = ctx
+
+
+    const promise = new Promise((resolve, reject) => {
+        upload(req, res, (err) => {
+            if (err) return reject(new FileUploadError())
+            resolve()
+        }) 
+    })
+
+    await promise
+    return await next()
+}
 
 
 
@@ -34,5 +54,14 @@ qiniu_router.get('ls', (ctx) => {
     return File.ls(user, params.tag)
 })
 
+
+
+// 文件上传
+qiniu_router.post('upload', upload_midd_ware, (ctx) => {
+    const {req, res} = ctx
+    
+    
+    return '文件上传成功'
+})
 
 module.exports = qiniu_router
