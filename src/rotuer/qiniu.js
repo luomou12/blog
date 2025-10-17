a5bdebbee17ffd3d9c154b41f5387d026ff0492c
L <3049152784@qq.com>
2025-10-17 19:07:51 +0800

    上传文件接口实现

--- src/rotuer/qiniu.js
+++ src/rotuer/qiniu.js
@@ -1,5 +1,6 @@
 const Router = require('koa-router');
 const File = require('../api/file.js');
+const QiniuFile = require('../api/qiniu_file.js')
 const { ParamError, FileUploadError } = require('../exceptions.js');
 
 
@@ -57,11 +58,34 @@ qiniu_router.get('ls', (ctx) => {
 
 
 // 文件上传
-qiniu_router.post('upload', upload_midd_ware, (ctx) => {
-    const {req, res} = ctx
+qiniu_router.post('upload', upload_midd_ware, async (ctx) => {
+    let tag = (ctx.query.tag || '/').trim( )
+    const file = ctx.req.file
+    const buffer = file.buffer
+    let file_name = file.originalname
     
+ 
+    // 补全路径
+    if (tag !== '/' && tag !== ' ') {
+        tag = tag.replace(/\\/g, '/').replace(/\/{2,}/g, '/')
+
+        file_name = [tag, file_name].join("/")
+        file_name = file_name.replace(/\//, '').replace(/\/{2,}/g, '/')
+
+    }
+
+
+    // 检测路径是否存在
+    const user = ctx.user
+    File.ls(user, tag)
+
+    // 上传远程云盘
+    const data = await QiniuFile.upload(file_name, buffer)
     
-    return '文件上传成功'
+    // 同步更新本地文件配置
+    File.upload_user_file(user, tag, file.originalname)
+    
+    return data
 })
 
 module.exports = qiniu_router
